9a591ebf9e856d6f52de5230c4d2f35ad9a1ac33
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-12-09 20:27:35 +0530

    copied to clipboard added

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, type, weight }) => {
+  const [alert, setAlert] = useState(false);
+
   const hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
+
+  function handleClick() {
+    navigator.clipboard.writeText(hex).then(() => {
+      setAlert(true);
+      setTimeout(() => {
+        setAlert(false);
+      }, 3000);
+    });
+  }
+
   return (
     <article
       className={`color ${type === "shade" && "color-light"}`}
       style={{ backgroundColor: hex }}
+      onClick={handleClick}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
